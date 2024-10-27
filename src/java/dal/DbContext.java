cad70fb3bb83aaa82c43779bb1be320dfcd305c4
namakariyassuke <email@example.com>
2024-10-27 19:37:08 +0700

    Test commnit 3rd

--- src/java/dal/DbContext.java
+++ src/java/dal/DbContext.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,30 +8,33 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author ASUS
+ * @author Admin
  */
 public abstract class DBContext<T> {
     protected Connection connection;
-    public DBContext()
-    {
+
+    public DBContext() {
         try {
+
             String user = "sa";
-            String pass = "123";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=SupportManagement;encrypt=true;trustServerCertificate=true;";
+            String pass = "long";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=SupportManagement";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public abstract void insert(T model);
-    public abstract void update(T model);
-    public abstract void delete(T model);
+
     public abstract ArrayList<T> list();
-    public static void main(String[] args) {
-        
-    }
+
+    public abstract T get(int id);
+
+    public abstract void insert(T model);
+
+    public abstract void update(T model);
+
+    public abstract void delete(T model);
+
 }
